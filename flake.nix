8381fb9c8b655e10d2dad5ee47bd7af984808ed7
Dolphin von Chips <td2505052@gmail.com>
2026-01-18 23:07:09 +0300

    fix(flake): reduce amount of dependencies, add app

--- flake.nix
+++ flake.nix
@@ -4,53 +4,79 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    oxalica.url = "github:oxalica/rust-overlay";
+    crane.url = "github:ipetkov/crane";
+    self.submodules = true;
   };
 
-  outputs = { self, nixpkgs, flake-utils, oxalica }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      crane,
+    }:
     with flake-utils.lib;
-    eachSystem allSystems (system:
+    eachSystem allSystems (
+      system:
       let
-        pkgs = nixpkgs.legacyPackages.${system}.extend oxalica.overlays.default;
-      in rec {
+        pkgs = nixpkgs.legacyPackages.${system};
+        craneLib = crane.mkLib pkgs;
+      in
+      {
+        packages = rec {
+          deadsync = craneLib.buildPackage rec {
+            src = ./.;
+            strictDeps = true;
 
-        packages = {
-          deadsync = let
-            rustPlatform = pkgs.makeRustPlatform {
-              cargo = pkgs.rust-bin.stable.latest.minimal;
-              rustc = pkgs.rust-bin.stable.latest.minimal;
-            };
-          in rustPlatform.buildRustPackage rec {
-            name = "deadsync";
-            src = self;
-            nativeBuildInputs = with pkgs; [ pkg-config ];
-            buildInputs = with pkgs; [
-                            alsa-lib.dev
-                            udev.dev 
-                            xorg.libX11
-                            xorg.libXrandr
-                            xorg.libXcursor
-                            xorg.libxcb
-                            xorg.libXi
-                            wayland
-                            libxkbcommon
-                            libxkbcommon.dev
-                            vulkan-loader
-                            vulkan-tools
-                            glfw
-                            libGL.dev
-                            libGL
-                            glew.dev
-                            glew
-                            egl-wayland
-                            egl-wayland.dev
-                            xorg.xf86videoamdgpu  # notice this line might not match your needs or desires
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              makeWrapper
             ];
-            cargoLock = { lockFile = ./Cargo.lock; };
+            buildInputs = with pkgs; [
+              alsa-lib
+              libGL
+              libxcursor
+              libxi
+              libxkbcommon
+              shaderc # NB: this should be in nativeBuildInputs, but otherwise shaderc-sys builds its own copy of shaderc
+              udev
+              vulkan-loader
+              wayland
+              xorg.libX11
+              xorg.libxcb
+            ];
+
+            doCheck = false;
+
             LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+            VK_LAYER_PATH = "${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d";
+            postInstall = ''
+              wrapProgram \
+                "$out/bin/deadsync" \
+                --set LD_LIBRARY_PATH ${LD_LIBRARY_PATH} \
+                --set VK_LAYER_PATH ${VK_LAYER_PATH}
+            '';
           };
+          default = deadsync;
         };
-        defaultPackage = packages.deadsync;
+
+        apps = rec {
+          deadsync = flake-utils.lib.mkApp {
+            drv = self.packages.${system}.deadsync;
+          };
+          default = deadsync;
+        };
+
+        devShells.default =
+          let
+            inherit (self.packages.${system}) deadsync;
+          in
+          craneLib.devShell {
+            inputsFrom = [ deadsync ];
+            inherit (deadsync) LD_LIBRARY_PATH VK_LAYER_PATH;
+          };
+
         formatter = pkgs.nixfmt;
-      });
+      }
+    );
 }
