50747676ab257c8139c5114752c4009f397723ef
adstep <stephensonadamj@gmail.com>
2026-03-08 12:53:05 -0700

    fix: persist all keyboard key mappings across restarts
    The parse_binding_token function only recognized ~43 hardcoded KeyCode
    variants when loading key mappings from the config file. Keys like
    Period, AltLeft, modifier keys, F-keys, digit keys, and many others
    would serialize correctly via Debug formatting but silently fail to
    deserialize on restart, causing those mappings to be lost.
    
    Extract keycode deserialization into a separate parse_keycode function
    using a macro to cover all ~150 winit KeyCode variants, ensuring every
    key that can be mapped in-game will round-trip through save/load.

--- src/config.rs
+++ src/config.rs
@@ -2336,68 +2336,72 @@ fn binding_to_token(binding: InputBinding) -> String {
 }
 
 #[inline(always)]
+fn parse_keycode(name: &str) -> Option<KeyCode> {
+    macro_rules! keycode_match {
+        ($input:expr, $( $name:ident ),* $(,)?) => {
+            match $input {
+                $( stringify!($name) => Some(KeyCode::$name), )*
+                _ => None,
+            }
+        };
+    }
+    keycode_match!(name,
+        // Writing system keys
+        Backquote, Backslash, BracketLeft, BracketRight, Comma,
+        Digit0, Digit1, Digit2, Digit3, Digit4,
+        Digit5, Digit6, Digit7, Digit8, Digit9,
+        Equal, IntlBackslash, IntlRo, IntlYen,
+        KeyA, KeyB, KeyC, KeyD, KeyE, KeyF, KeyG, KeyH, KeyI,
+        KeyJ, KeyK, KeyL, KeyM, KeyN, KeyO, KeyP, KeyQ, KeyR,
+        KeyS, KeyT, KeyU, KeyV, KeyW, KeyX, KeyY, KeyZ,
+        Minus, Period, Quote, Semicolon, Slash,
+        // Modifier keys
+        AltLeft, AltRight, Backspace, CapsLock, ContextMenu,
+        ControlLeft, ControlRight, Enter, SuperLeft, SuperRight,
+        ShiftLeft, ShiftRight, Space, Tab,
+        // IME keys
+        Convert, KanaMode, Lang1, Lang2, Lang3, Lang4, Lang5, NonConvert,
+        // Navigation keys
+        Delete, End, Help, Home, Insert, PageDown, PageUp,
+        ArrowDown, ArrowLeft, ArrowRight, ArrowUp,
+        // Numpad keys
+        NumLock,
+        Numpad0, Numpad1, Numpad2, Numpad3, Numpad4,
+        Numpad5, Numpad6, Numpad7, Numpad8, Numpad9,
+        NumpadAdd, NumpadBackspace, NumpadClear, NumpadClearEntry,
+        NumpadComma, NumpadDecimal, NumpadDivide, NumpadEnter,
+        NumpadEqual, NumpadHash,
+        NumpadMemoryAdd, NumpadMemoryClear, NumpadMemoryRecall,
+        NumpadMemoryStore, NumpadMemorySubtract,
+        NumpadMultiply, NumpadParenLeft, NumpadParenRight,
+        NumpadStar, NumpadSubtract,
+        // Function keys
+        Escape, Fn, FnLock, PrintScreen, ScrollLock, Pause,
+        // Media / browser keys
+        BrowserBack, BrowserFavorites, BrowserForward, BrowserHome,
+        BrowserRefresh, BrowserSearch, BrowserStop,
+        Eject, LaunchApp1, LaunchApp2, LaunchMail,
+        MediaPlayPause, MediaSelect, MediaStop,
+        MediaTrackNext, MediaTrackPrevious,
+        Power, Sleep, AudioVolumeDown, AudioVolumeMute, AudioVolumeUp, WakeUp,
+        // Misc
+        Meta, Hyper, Turbo, Abort, Resume, Suspend,
+        Again, Copy, Cut, Find, Open, Paste, Props, Select, Undo,
+        Hiragana, Katakana,
+        // F-keys
+        F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
+        F13, F14, F15, F16, F17, F18, F19, F20, F21, F22, F23, F24,
+        F25, F26, F27, F28, F29, F30, F31, F32, F33, F34, F35,
+    )
+}
+
 fn parse_binding_token(tok: &str) -> Option<InputBinding> {
     let t = tok.trim();
     // Keyboard
     if let Some(rest) = t.strip_prefix("KeyCode::") {
-        let code = match rest {
-            // Special keys
-            "Enter" => KeyCode::Enter,
-            "Escape" => KeyCode::Escape,
-            "ArrowUp" => KeyCode::ArrowUp,
-            "ArrowDown" => KeyCode::ArrowDown,
-            "ArrowLeft" => KeyCode::ArrowLeft,
-            "ArrowRight" => KeyCode::ArrowRight,
-            "Slash" => KeyCode::Slash,
-            // Numpad keys
-            "Numpad0" => KeyCode::Numpad0,
-            "Numpad1" => KeyCode::Numpad1,
-            "Numpad2" => KeyCode::Numpad2,
-            "Numpad3" => KeyCode::Numpad3,
-            "Numpad4" => KeyCode::Numpad4,
-            "Numpad5" => KeyCode::Numpad5,
-            "Numpad6" => KeyCode::Numpad6,
-            "Numpad7" => KeyCode::Numpad7,
-            "Numpad8" => KeyCode::Numpad8,
-            "Numpad9" => KeyCode::Numpad9,
-            "NumpadAdd" => KeyCode::NumpadAdd,
-            "NumpadDivide" => KeyCode::NumpadDivide,
-            "NumpadDecimal" => KeyCode::NumpadDecimal,
-            "NumpadComma" => KeyCode::NumpadComma,
-            "NumpadEnter" => KeyCode::NumpadEnter,
-            "NumpadEqual" => KeyCode::NumpadEqual,
-            "NumpadMultiply" => KeyCode::NumpadMultiply,
-            "NumpadSubtract" => KeyCode::NumpadSubtract,
-            // Letter keys A-Z
-            "KeyA" => KeyCode::KeyA,
-            "KeyB" => KeyCode::KeyB,
-            "KeyC" => KeyCode::KeyC,
-            "KeyD" => KeyCode::KeyD,
-            "KeyE" => KeyCode::KeyE,
-            "KeyF" => KeyCode::KeyF,
-            "KeyG" => KeyCode::KeyG,
-            "KeyH" => KeyCode::KeyH,
-            "KeyI" => KeyCode::KeyI,
-            "KeyJ" => KeyCode::KeyJ,
-            "KeyK" => KeyCode::KeyK,
-            "KeyL" => KeyCode::KeyL,
-            "KeyM" => KeyCode::KeyM,
-            "KeyN" => KeyCode::KeyN,
-            "KeyO" => KeyCode::KeyO,
-            "KeyP" => KeyCode::KeyP,
-            "KeyQ" => KeyCode::KeyQ,
-            "KeyR" => KeyCode::KeyR,
-            "KeyS" => KeyCode::KeyS,
-            "KeyT" => KeyCode::KeyT,
-            "KeyU" => KeyCode::KeyU,
-            "KeyV" => KeyCode::KeyV,
-            "KeyW" => KeyCode::KeyW,
-            "KeyX" => KeyCode::KeyX,
-            "KeyY" => KeyCode::KeyY,
-            "KeyZ" => KeyCode::KeyZ,
-            _ => return None,
-        };
-        return Some(InputBinding::Key(code));
+        if let Some(code) = parse_keycode(rest) {
+            return Some(InputBinding::Key(code));
+        }
     }
 
     // Gamepad low-level code binding:
